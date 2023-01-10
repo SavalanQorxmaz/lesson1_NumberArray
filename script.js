b566000648a33a5780b681d289e1e3e4f8e1faae
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-01-10 17:07:39 +0400

    stringReverse and arrayCompare

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-`----------------- SWAP EACH OTHER WITH THIRD VARIABLE----------------------------------- ` 
+// ----------------- SWAP EACH OTHER WITH THIRD VARIABLE----------------------------------- ` 
  
 
 const swap3 = () => {
@@ -27,7 +27,9 @@ const swap3 = () => {
 }
 
 //  swap3()
-`---------------- SWAP EACH OTHER WITHOUT THIRD VARIABLE---------------------------------` 
+
+
+// ---------------- SWAP EACH OTHER WITHOUT THIRD VARIABLE--------------------------------- 
 
 const swap = () => {
 
@@ -52,3 +54,120 @@ const swap = () => {
  }
 
 //  swap()
+
+
+
+// ---------------- STRING REVERSE---------------------------------
+
+
+
+
+const wordReverse = () => {
+
+    
+let word =prompt("SOZ DAXIL ET");
+console.log(`DAXIL EDILEN SOZ        ${word}`);
+
+let wordArray = word.split("");
+console.log(`SOZUN MASSIV SHEKLI     ${wordArray}`);
+
+wordArray.reverse();
+console.log(`CEVRILMISH MASSIV       ${wordArray}`);
+
+let reversedWord = "";
+
+for(let i=0; i<wordArray.length; i++){
+    reversedWord += wordArray[i]
+}
+console.log(`CEVRILMISH SOZ          ${reversedWord}`);
+
+}
+
+// wordReverse()
+
+
+
+
+// ___________________ARRAY COMPARE__________________
+
+
+
+const arrayCompare = () => {
+
+    let string1 = prompt(`BIRINCI MASSIV:
+    ELEMENTLER ARASINDA + ISHARESI QOY`);
+    let string2 = prompt(`IKINCI MASSIV:
+    ELEMENTLER ARASINDA + ISHARESI QOY`);
+    
+    console.log("DAXIL EDILEN SETIRLER")
+    console.log(string1);
+    console.log(string2);
+    
+    let array1 = string1.split("+");
+    let array2 =string2.split("+");
+    
+    console.log("SETIRLERIN MASSIVE CHEVRILMESI")
+    console.log(array1);
+    console.log(array2);
+    
+    for(var i=0;i<array1.length;i++){
+        array1[i] = Number(array1[i]);   
+    }
+    for(var i=0;i<array2.length;i++){
+        array2[i] = Number(array2[i]);
+    }
+    
+    console.log("SETIRLERIN NUMBER TIPINE DEYISHDIRILMESI")
+    console.log(array1);
+    console.log(array2);
+    
+    for(var i=0;i<array1.length;i++){
+        if(isNaN(array1[i])||array1[i] == ""){
+            array1.splice(i,1);
+            i--;
+        }
+    }
+    
+    for(var i=0;i<array2.length;i++){
+        if(isNaN(array2[i])||array2[i] == ""){
+            array2.splice(i,1);
+            i--;
+        }
+    }
+    
+    console.log("NAN VE EMPTY ELEMENTLERIN XARIC EDILMESI")
+    console.log(array1);
+    console.log(array2);
+    
+    let sameElementsArray = [];
+    
+    let len =0;
+    
+    array1.length <= array2.length ? len = array1.length : len = array2.length;
+    
+    
+    
+    
+    for(var i = 0;i<array1.length; i++){
+    
+        for(var j=0;j<array2.length; j++){
+    
+            if(array1[i] == array2[j]){
+                sameElementsArray.push(array1[i]);
+                array1.splice(i,1);
+                array2.splice(j,1)
+                j = 0;
+                i--;
+            }
+    
+        }
+    }
+    
+    
+    console.log("BIRINCI MASSIVIN FERQLI ELEMENTLERI:   ",array1);
+    console.log("IKINCI MASSIVIN FERQLI ELEMENTLERI:    ",array2);
+    console.log("MASSIVLERIN ORTAQ ELEMENTLERI:         ",sameElementsArray);
+
+}
+
+// arrayCompare()
